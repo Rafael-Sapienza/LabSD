ed8e4933d08f6858918d45bd1824f922d883b777
Rafael <rafaelsapienzapinheiro@gmail.com>
2025-01-29 00:00:20 -0300

    Experimento 7 está pronto para ser validado!

--- experimento7/testbenchMaquinaMoore.vhdl
+++ experimento7/testbenchMaquinaMoore.vhdl
@@ -10,86 +10,155 @@ END TBMaquinaDeVendas;
 
 ARCHITECTURE TBMAquinaDeVendas_ARCH OF TBMaquinaDeVendas IS
 BEGIN
-	process 
-    begin
-        while true loop
-            clock <= '0';
-            wait for 10 ns;
-            clock <= '1';
-            wait for 10 ns;
-        end loop;
-    end process;
-
+    process
 	begin
 		report "Iniciando teste..." severity NOTE;
         --Espera um ciclo de estabilidade
         wait for 20 ns;
         --Nenhuma moeda foi inserida
         A <= "11";
-		wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-		wait for 20 ns;
+		clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Nenhuma moeda foi inserida
         A <= "00";
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Pedindo troco
         A <= "11";
-        wait for 20 ns;
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-		wait for 20 ns;
+		clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-		wait for 20 ns;
+		clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Nenhuma moeda foi inserida
         A <= "00";
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Pedindo troco
         A <= "11";
-        wait for 20 ns;
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-		wait for 20 ns;
+		clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-		wait for 20 ns;
+		clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-		wait for 20 ns;
+		clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Pedindo troco
         A <= "11";
-        wait for 20 ns;
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 50 centavos
         A <= "10";
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 50 centavos/ libera sem troco
         A <= "10";
-        wait for 20 ns;
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 50 centavos
         A <= "10";
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos/ libera sem troco
         A <= "01";
-        wait for 20 ns;
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 25 centavos
         A <= "01";
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
         --Inserindo 50 centavos
         A <= "10";
-        wait for 20 ns;
-        --Inserindo 50 centavos/ libera sem troco
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        --Inserindo 50 centavos/ libera com troco
         A <= "10";
-        wait for 20 ns;
-        wait for 20 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
+        clock <= '0';
+        wait for 10 ns;
+        clock <= '1';
+        wait for 10 ns;
 
 		report "Teste finalizado!" severity NOTE;
 		wait;
--- experimento7/topmoduleMaquinaMoore.vhdl
+++ experimento7/topmoduleMaquinaMoore.vhdl
@@ -9,18 +9,19 @@ architecture topmoduleMaquinaDeVendas_arch of topmoduleMaquinaDeVendas is
     component maquinaDeVendas is
         port(clock :in std_logic;
             A: in std_logic_vector(1 downto 0);
-            P,M_25,M_50: out std_logic;
+            P,M_25,M_50: out std_logic);
     end component;
 
-    component TBMaquinaDeVendas IS
-    PORT(clock: in std_logic;
+    component TBMaquinaDeVendas is
+    PORT(clock: out std_logic;
         A: out std_logic_vector(1 downto 0));
     end component;
 
-    signal A: std_logic_vector(1 downto 0);
-    signal P,M_25,M_50:  std_logic;
+    signal A_v2: std_logic_vector(1 downto 0);
+    signal clock_v2: std_logic;
+    signal P_v2,M_25_v2,M_50_v2:  std_logic;
 
 begin
-    U0: maquinaDeVendas port map(clock,A,P,M_25,M_50);
-    U1: TBMaquinaDeVendas port map(clock,A);
-end topmoduleMaquinaDeVendas;
+    U0: maquinaDeVendas port map(clock_v2,A_v2,P_v2,M_25_v2,M_50_v2);
+    U1: TBMaquinaDeVendas port map(clock_v2,A_v2);
+end topmoduleMaquinaDeVendas_arch;
